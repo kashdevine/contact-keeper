4addc3819827cafa9a856f4ab58af8d60ffa6307
kashdevine <kasheen.mowatt@gmail.com>
2021-07-27 20:03:18 -0700

    Express Server setup with inital routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res)=> res.json({msg:'Welcome to the contact keeper api...'}));
+
+
+// Define Routes
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+// Setup Port
+
+const PORT = process.env.PORT || 5000;
+
+// Start Listening to the port
+app.listen(PORT, ()=> console.log(`Server Started on port ${PORT}`));
