8b8809cdb59708d96180cd37593238db14b5f5d3
kashdevine <kasheen.mowatt@gmail.com>
2021-11-07 13:34:03 -0800

    Wired Register component to backend

--- client/src/App.js
+++ client/src/App.js
@@ -4,30 +4,35 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
-import Register from './components/auth/register';
-import Login from './components/auth/login';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
+
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
-
+import AlertState from './context/alert/AlertState';
 
 const App = () =>  {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar/>
-          </Fragment>
-          <div className="container">
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/about' component={About}/>
-            <Route exact path='/register' component={Register}/>
-            <Route exact path='/login' component={Login}/>
-          </Switch>
-          </div>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar/>
+            </Fragment>
+            <div className="container">
+            <Alerts/>
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/about' component={About}/>
+              <Route exact path='/register' component={Register}/>
+              <Route exact path='/login' component={Login}/>
+            </Switch>
+            </div>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
